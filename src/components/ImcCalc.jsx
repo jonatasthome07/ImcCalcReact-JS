2eb0505242bf97cef3386272dc1fc272706590dd
Jônatas <jonatasthome04@gmail.com>
2025-08-22 00:47:59 -0300

    UseState and controlled inputs was added

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,7 +1,18 @@
 import "./ImcCalc.css"
 import Button from "./Button"
+import { useState } from "react"
 
 const ImcCalc = () =>{
+
+    const [height, setHeigth] = useState("")
+    const [weight, setWeight] = useState("")
+
+    const clearForm = (e) =>{
+        e.preventDefault()
+        setHeigth("")
+        setWeight("")
+    }
+
     return (
         <>
          <div id="calc-container">
@@ -10,18 +21,20 @@ const ImcCalc = () =>{
                 <div className="form-inputs">
                     <div className="form-control">
                         <label htmlFor="height">Height: </label>
-                        <input type="text" name="height" id="height" placeholder="Example 1,75"/>
+                        <input type="text" name="height" id="height" placeholder="Example 1,75"
+                        onChange={(e)=> setHeigth(e.target.value)} value={height}/>
                     </div>
                     <div className="form-control">
                         <label htmlFor="weight">Weight: </label>
-                        <input type="text" name="weight" id="weight" placeholder="Example 70,5"/>
+                        <input type="text" name="weight" id="weight" placeholder="Example 70,5" 
+                        onChange={(e) => setWeight(e.target.value)} value={weight}/>
                     </div>
                 </div>
             </form>
         </div>
         <div className="action-control">
             <Button id="calc-btn" text={"Calculate"}></Button>
-            <Button id="clear-btn" text={"Reset"}></Button>
+            <Button id="clear-btn" text={"Reset"} action={clearForm}></Button>
         </div>
         </>
     )
